3f600ec6f9020b0c04d09729ff5ed2fda766a51d
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-28 15:40:50 -0600

    Add form for new artwork

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -2,4 +2,17 @@ class ArtworksController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+    artwork = Artwork.new(artwork_params)
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:name, :description)
+  end
 end
